a29857dde15d03021cca1aeb3ad4ac822526bb5e
saifnihad300 <hossainnihad300@gmail.com>
2025-03-27 20:07:52 +0600

    deploy

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ import cors from 'cors'
 import connectdb from './mongodb/connect.js'
 import userRouter from './routes/user.routes.js'
 import propertyRouter from './routes/property.routes.js'
+const path = require('path');
 
 
 dotenv.config()
@@ -11,6 +12,11 @@ dotenv.config()
 const app = express()
 app.use(cors())
 app.use(express.json({limit: '50mb'}));
+
+app.use(express.static(path.join(__dirname, 'dist')));
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
 app.get('/', (req, res) =>{
     res.send({message: "Hello world!"});
