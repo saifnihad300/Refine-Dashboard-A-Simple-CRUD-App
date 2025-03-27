2bbca37cf988dcf1c71c87c121e12c8763d0fead
saifnihad300 <hossainnihad300@gmail.com>
2025-03-27 20:48:42 +0600

    save

--- server/index.js
+++ server/index.js
@@ -4,7 +4,11 @@ import cors from 'cors'
 import connectdb from './mongodb/connect.js'
 import userRouter from './routes/user.routes.js'
 import propertyRouter from './routes/property.routes.js'
-const path = require('path');
+import path from 'path'
+import {fileURLToPath} from 'url'
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 
 dotenv.config()
