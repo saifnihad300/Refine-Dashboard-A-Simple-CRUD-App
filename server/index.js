1c73b1e9e254236aa4d3df6036b124131c58af0d
saifnihad300 <hossainnihad300@gmail.com>
2025-04-05 23:42:01 +0600

    make changes

--- server/index.js
+++ server/index.js
@@ -4,23 +4,12 @@ import cors from 'cors'
 import connectdb from './mongodb/connect.js'
 import userRouter from './routes/user.routes.js'
 import propertyRouter from './routes/property.routes.js'
-import path from 'path'
-import {fileURLToPath} from 'url'
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
 
 dotenv.config()
 
 const app = express()
 app.use(cors())
 app.use(express.json({limit: '50mb'}));
-
-app.use(express.static(path.join(__dirname, 'dist')));
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
 
 app.get('/', (req, res) =>{
     res.send({message: "Hello world!"});
